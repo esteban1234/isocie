b704e32a1b4d1c3cfcded869bdfe29caee615bda
champiiy <albert1047@hotmail.com>
2017-06-21 18:33:45 -0500

    RESPONSIVO V1.1

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="https://pingendo.github.io/templates/app/theme.css" type="text/css">
+  <link rel="stylesheet" href="css/responsivo.css">
   <title>ISOCIE</title>
   <meta name="keywords" content="contabilidad, impuestos, facturación digital, facturación electrónica, soluciones, planes, impuestos, regímenes fiscales">
   <meta name="description" content="ISOCIE Tus impuestos y contabilidad online, sin complicaciones.">
@@ -38,23 +39,24 @@
 
   </nav>
 
-  <div class="cover d-flex align-items-center pt-5 bg-primary" id="inicio">
+  <div class="cover align-items-center pt-5 bg-primary" id="inicio">
     <div class="container">
       <div class="row">
-        <div class="col-md-6 text-md-left align-self-center my-5">
-          <h1 class="text-white display-1">Contabilidad</h1>
+        <div class="col-xs-12 col-md-6 text-md-left align-self-center">
+          <h1 class="text-white display-1 new-tamano">Contabilidad</h1>
           <h4 class="text-white">Resultados para su empresa, garantizados.</h4>
           <div class="row mt-5">
-            <div class="col-md-5 col-6">
+            <div class="col-xs-12 col-md-5 margin-bottom">
             <a href="#contacto"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
             </div>
-            <div class="col-md-5 col-6">
+            <div class="col-xs-12 col-md-5">
             <a href="#servicios"><button type="button" class="btn btn-danger btn-lg">SERVICIOS</button></a>
             </div>
           </div>
         </div>
-        <div class="col-md-6">
-          <img class="img-fluid d-block mx-auto" src="img/isocie.png"> </div>
+        <div class="col-xs-12 col-md-6">
+          <img class="img-fluid d-block mx-auto" src="img/isocie.png">
+        </div>
       </div>
     </div>
   </div>
@@ -62,16 +64,17 @@
   <div class="section py-5 text-md-left" id="nosotros">
     <div class="container">
       <div class="row">
-        <div class="col-md-7 push-md-5 my-3 align-self-center">
+        <div class="col-xs-12 col-md-7 push-md-5 my-3 align-self-center">
           <h2 class="text-danger my-3">Conocenos</h2>
           <p class="lead">En ISOCIE ofrecemos soluciones integrales de acuerdo a sus necesidades, con servicios innovadores y con capacitaciones constantes al personal. En ISOCIE siempre buscamos la mejor calidad, rentabilidad y confianza en nuestros servicios, para alcanzar siempre la satisfacción total de nuestro cliente. </p>
           <a href="features.html" class="btn btn-outline-danger btn-lg">Contáctanos</a>
         </div>
-        <div class="col-md-5 pull-md-7 my-3">
+        <div class="col-xs-12 col-md-5 pull-md-7 my-3">
           <img src="img/legal.jpg" class="img-fluid d-block mx-auto img-thumbnail"> </div>
       </div>
     </div>
   </div>
+
   <div class="py-5 text-left">
     <div class="container">
       <div class="row">
@@ -85,6 +88,7 @@
       </div>
     </div>
   </div>
+
   <div class="section py-5" id="servicios">
     <div class="container">
       <div class="row">
@@ -95,49 +99,50 @@
       <div class="row">
         <div class="align-self-center text-right col-lg-4 col-md-6">
           <div class="row my-5">
-            <div class="col-10">
-              <h4>AUDITORÍA DE CONTROL INTERNO</h4>
-              <p>Control e información de la operaciones, puesto que permite el manejo adecuado de los bienes, funciones e información de una empresa determinada, con el fin de generar una indicación confiable de su situación y sus operaciones en el mercado.</p>
+            <div class="col-xs-12 col-lg-10">
+              <h4 class="new-font-tamano">AUDITORÍA DE CONTROL INTERNO</h4>
+              <p class="alinear">Control e información de la operaciones, puesto que permite el manejo adecuado de los bienes, funciones e información de una empresa determinada, con el fin de generar una indicación confiable de su situación y sus operaciones en el mercado.</p>
             </div>
-            <div class="col-2"><i class="d-block fa fa-3x fa-columns"></i></div>
+            <div class="col-xs-12 col-lg-2 oculto"><i class="d-block fa fa-3x fa-columns"></i></div>
           </div>
           <div class="row my-5">
-            <div class="col-10">
-              <h4>SERVICIOS CONTABLES</h4>
-              <p>Están diseñados para que se constituyan en un verdadero soporte administrativo.</p>
+            <div class="ccol-xs-12 col-lg-10">
+              <h4 class="new-font-tamano">SERVICIOS CONTABLES</h4>
+              <p class="alinear">Están diseñados para que se constituyan en un verdadero soporte administrativo.</p>
             </div>
-            <div class="col-2"><i class="d-block fa  fa-3x fa-circle-thin"></i></div>
+            <div class="col-xs-12 col-lg-2 oculto"><i class="d-block fa  fa-3x fa-circle-thin"></i></div>
           </div>
           <div class="row my-5">
-            <div class="col-10">
-              <h4>AUDITORÍA LEGAL</h4>
-              <p>Obtener información sobre la cual se pueda soportar una opinión de la situación del estado legal de una empresa.</p>
+            <div class="col-xs-12 col-lg-10">
+              <h4 class="new-font-tamano">AUDITORÍA LEGAL</h4>
+              <p class="alinear">Obtener información sobre la cual se pueda soportar una opinión de la situación del estado legal de una empresa.</p>
             </div>
-            <div class="col-2"><i class="d-block fa  fa-3x fa-comment-o"></i></div>
+            <div class="col-xs-12 col-lg-2 oculto"><i class="d-block fa  fa-3x fa-comment-o"></i></div>
           </div>
         </div>
         <div class="my-3 col-lg-4 col-md-6">
-          <img class="img-fluid d-block mx-auto" src="img/serv.png"> </div>
+          <img class="img-fluid d-block mx-auto" src="img/serv.png">
+        </div>
         <div class="align-self-center text-left col-lg-4 col-md-12">
           <div class="row my-5">
-            <div class="col-3"><i class="d-block fa fa-battery-empty fa-3x"></i></div>
-            <div class="col-9">
-              <h4>AUDITORÍA FINANCIERA</h4>
-              <p>Examina a los estados financieros y a través de ellos las operaciones financieras realizadas por el ente contable.</p>
+            <div class="col-xs-12 col-lg-3 oculto"><i class="d-block fa fa-battery-empty fa-3x"></i></div>
+            <div class="col-xs-12 col-lg-9">
+              <h4 class="new-font-tamano">AUDITORÍA FINANCIERA</h4>
+              <p class="alinear">Examina a los estados financieros y a través de ellos las operaciones financieras realizadas por el ente contable.</p>
             </div>
           </div>
           <div class="row my-5">
-            <div class="col-3"><i class="d-block mx-auto fa  fa-3x fa-clone"></i></div>
-            <div class="col-9">
-              <h4>ESTRATEGIA FISCAL</h4>
-              <p>Son las acciones de orden preventivo que se han organizado en la planeación de los negocios y empresas.</p>
+            <div class="col-xs-12 col-lg-3 oculto"><i class="d-block mx-auto fa  fa-3x fa-clone"></i></div>
+            <div class="col-xs-12 col-lg-9">
+              <h4 class="new-font-tamano">ESTRATEGIA FISCAL</h4>
+              <p class="alinear">Son las acciones de orden preventivo que se han organizado en la planeación de los negocios y empresas.</p>
             </div>
           </div>
           <div class="row my-5">
-            <div class="col-3"><i class="d-block fa  fa-3x fa-hourglass-half"></i></div>
-            <div class="col-9">
-              <h4>LITIGIO FISCAL Y ADMINISTRATIVO</h4>
-              <p>Comprende la representación de nuestros clientes en todo tipo de procedimientos contenciosos relacionados con la actividad del Estado.</p>
+            <div class="col-xs-12 col-lg-3 oculto"><i class="d-block fa  fa-3x fa-hourglass-half"></i></div>
+            <div class="col-xs-12 col-lg-9">
+              <h4 class="new-font-tamano">LITIGIO FISCAL Y ADMINISTRATIVO</h4>
+              <p class="alinear">Comprende la representación de nuestros clientes en todo tipo de procedimientos contenciosos relacionados con la actividad del Estado.</p>
             </div>
           </div>
         </div>
@@ -154,7 +159,7 @@
             <form class="form-horizontal" action="" method="post" >
             <fieldset>
               <h1 class="text-center">Formulario de contacto</h1 >
-      
+
               <!-- Name input-->
               <div class="form-group">
                 <label class="col-md-5 control-label" for="name">Nombre completo</label>
@@ -162,7 +167,7 @@
                   <input id="name" name="name" type="text" placeholder="Escribe tu Nombre Completo" class="form-control">
                 </div>
               </div>
-      
+
               <!-- Email input-->
               <div class="form-group">
                 <label class="col-md-3 control-label" for="email">Correo</label>
@@ -177,7 +182,7 @@
                   <input id="email" name="email" type="text" placeholder="Escribe tu Teléfono" class="form-control">
                 </div>
               </div>
-      
+
               <!-- Message body -->
               <div class="form-group">
                 <label class="col-md-3 control-label" for="message">Mensaje</label>
@@ -185,7 +190,7 @@
                   <textarea class="form-control" id="message" name="message" placeholder="Escribe tu Mensaje" rows="5"></textarea>
                 </div>
               </div>
-      
+
               <!-- Form actions -->
               <div class="form-group">
                 <div class="col-md-12 text-right">
@@ -205,21 +210,21 @@
   <div class="py-5 bg-info" id="#">
     <div class="container">
       <div class="row">
-        <div class="col-4 col-md-3 align-self-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 align-self-center">
           <a href="#" target="blank"><i class="fa fa-fw fa-clock-o fa-3x text-white"></i>Lun-Vie de 9 am a 6 pm</a>
         </div>
-        <div class="col-4 col-md-3 align-self-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 align-self-center">
           <a href="#" target="blank"><i class="fa fa-fw fa-envelope-o fa-3x text-white"></i>info@isocie.com</a>
         </div>
-        <div class="col-4 col-md-3 align-self-center">
+        <div class="col-lg-4 col-md-4 col-sm-4 align-self-center">
           <a href="#" target="blank"><i class="fa fa-fw fa-phone text-white fa-3x"></i>PENDIENTE</a>
         </div>
-        <div class="col-4 col-md-3 align-self-center">
+        <div class="col-lg-4 col-md-12 col-sm-12 align-self-center margin">
           <a href="#" target="blank"><i class="fa fa-fw fa-map-marker text-white fa-3x"></i>C. del Zorro #8 entre C. Tigre y Venado, Col. Boulevard del Bosque.  C.P. 30747 en Tapachula, Chiapas</a>
         </div>
       </div>
       <div class="row">
-        <div class="col-md-12 my-3">
+        <div class="col-xs-12 col-md-12 my-3">
           <p>© Copyright 2017 ISOCIE - TODOS LOS DERECHOS RESERVADOS. </p>
         </div>
       </div>
