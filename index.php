071a6b746e868e8782c36de170493807a6d0f22f
esteban1234 <tva_libra@hotmail.com>
2017-06-22 10:22:09 -0500

    codigo de verificacion

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <head>
   <meta charset="utf-8">
+  <meta name="google-site-verification" content="kmMJLJIf2pmsJE7vohKB6YbeJHTftEc_YuyPvDiYxxg" />
   <link rel="icon" type="image/png" href="img/favicon.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
